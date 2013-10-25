b4dd5773d79a3452c9767e969e2b1a70ccdeb582
RemyD <remyd@civx.us>
2013-10-25 17:01:46 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
-from setuptools import setup
+#!/bin/env python
+# -*- coding: utf8 -*-
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+
+from setuptools import setup, find_packages
+
+version = "0.5.0"
+
+setup(
+    name="hackshackersrocsite",
+    version=version,
+    description="website for hacks/hackers rochester",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Topic :: Education :: Computer Aided Instruction (CAI)",
+    ],
+    keywords="",
+    author="Remy DeCausemaker",
+    author_email="remyd@civx.us",
+    url="http://hackshackers-rochester.rhcloud.com",
+    license="Apache2.0",
+    packages=find_packages(
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "flask",
+        "mako",
+        "flask-mako",
+        "flask-markdown",
+    ],
+    #TODO: Deal with entry_points
+    #entry_points="""
+    #[console_scripts]
+    #pythong = pythong.util:parse_args
+    #"""
+)
